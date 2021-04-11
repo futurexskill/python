d439d88737e61900bdf6080adc92f2a3d1d5a03c
futureXskill <54407776+futurexskill@users.noreply.github.com>
2021-04-11 08:23:31 -0700

    Add files via upload

---
+++ PythonCollections/main.py
@@ -0,0 +1,22 @@
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    my_list = [1,2,3,4]
+    my_list.append(5)
+    my_list.pop(1)
+    print("My List")
+    print(my_list)
+    print(my_list[2])
+    my_tuple = (1,2,3,4)
+    print("My tuple")
+    print(my_tuple)
+    print(my_tuple[2])
+    my_set = {1,2,3,4}
+    print(my_set)
+    print(my_set(2))
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
